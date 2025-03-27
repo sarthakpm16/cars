a4a5de57a1f59efb0c6f7857882c873729312063
sarthakpm16 <sarthakpalmahajan@gmail.com>
2025-03-27 02:31:49 -0400

    changes to main

--- cars/Main.java
+++ cars/Main.java
@@ -1,11 +1,12 @@
 package cars;
 
 public class Main {
-    
     public static void main(String[] args) {
        Car c = new Tesla();
-       
        c.beep();
+
+       Car a = new Camaro();
+        a.beep();
     }
 
 }
