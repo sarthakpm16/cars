47347780c587eafa6607e6ac99f1d807145b2894
sarthakpm16 <sarthakpalmahajan@gmail.com>
2025-03-27 02:53:39 -0400

    added mustang & camaro

--- cars/Main.java
+++ cars/Main.java
@@ -4,8 +4,13 @@ public class Main {
     
     public static void main(String[] args) {
        Car c = new Tesla();
-       
        c.beep();
+
+       Car m = new Mustang();
+         m.beep();
+
+         Car a = new Camaro();
+            a.beep();
     }
 
 }
